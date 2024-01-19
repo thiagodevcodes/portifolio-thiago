62c5db11de08690b3e41432bc45cbf37fdef34e9
thiago <euthiagosrodrigues@gmail.com>
2024-01-19 18:11:33 -0300

    feat: adicionar icone do site e titulo

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,6 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-br">
+      <title>Porfifolio - Thiago Silva</title>
+      <link rel="icon" href="/img/tag-html.png" type="image/x-icon"></link>
       <Head />
       <body>
         <Main />
