6db73ddfcf28ab18d971485de4d2a48c9b9ab96f
thiago <euthiagosrodrigues@gmail.com>
2024-02-07 03:26:09 -0300

    feat: add slider

--- src/components/CardTech/index.jsx
+++ src/components/CardTech/index.jsx
@@ -1,12 +1,32 @@
 import styles from "./CardTech.module.css"
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function CardTech({ title, description, img, children}) {
+    const [larguraDaTela, setLarguraDaTela] = useState(0);
+
+    useEffect(() => {
+        const handleResize = () => {
+          setLarguraDaTela(window.innerWidth);
+        };
+    
+        if (typeof window !== 'undefined') {
+          window.addEventListener('resize', handleResize);
+    
+          setLarguraDaTela(window.innerWidth);
+    
+          return () => {
+            window.removeEventListener('resize', handleResize);
+          };
+        }
+      }, [])
+
     return (
         <div className={styles.container}>
             <div>
-                <Image alt="" width={250} height={120} src={img}/>
+                {larguraDaTela > 915 ? <Image alt="" width={250} height={120} src={img}/> : null }
+                
                 <h2>{title}</h2>
                 <p>{description}</p>
             </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,10 +9,37 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useEffect, useState } from "react";
 import MobileMenu from "@/components/MobileMenu";
+import Slider from "react-slick";
 
 export default function Home() {
   const [larguraDaTela, setLarguraDaTela] = useState(0);
   const [active, setActive] = useState(false);
+
+  let settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -35,23 +62,35 @@ export default function Home() {
       <Head>
         <title>Portifolio - Thiago Silva</title>
         <link rel="icon" href="/img/tag-html.png" type="image/x-icon"></link>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
       </Head>
+
       <Header>
-        { larguraDaTela > 915 ? <Nav/> : <MobileButton setActive={setActive} active={active}/> }   
+        {larguraDaTela > 915 ? <Nav /> : <MobileButton setActive={setActive} active={active} />}
       </Header>
 
-      { larguraDaTela < 915 && <MobileMenu setActive={setActive} active={active}/> }
-      
+      {larguraDaTela < 915 && <MobileMenu setActive={setActive} active={active} />}
+
       <Section id="about" color="#1b1b1b" height={larguraDaTela > 915 ? "100vh" : null}>
         <div className={styles.containerAbout}>
-          <Image alt="foto de thiago" className={styles.img} width={300} height={300} src="/img/foto.jpg" priority/>
+          <Image alt="foto de thiago" className={styles.img} width={300} height={300} src="/img/foto.jpg" priority />
           <div className={styles.aboutTexts}>
             <h2 className={styles.colorBlue}>OLÁ, SOU</h2>
             <h2 className={styles.colorWhite}>THIAGO SILVA RODRIGUES</h2>
             <h2 className={styles.colorBlue}>DESENVOLVEDOR WEB</h2>
-            <p>Sou amante da área da tecnologia e este é meu portifólio com 
-              alguns mini projetos que desenvolvi, me formei recentemente em análise e desenvolvimento de sistemas, estou sempre buscando evoluir na área e 
-              tenho disposição para aprender e seguir aprimorando meus conhecimentos, 
+            <p>Sou amante da área da tecnologia e este é meu portifólio com
+              alguns mini projetos que desenvolvi, me formei recentemente em análise e desenvolvimento de sistemas, estou sempre buscando evoluir na área e
+              tenho disposição para aprender e seguir aprimorando meus conhecimentos,
               atualmente com foco no desenvolvimento web e estou à procura de uma oportunidade.
             </p>
           </div>
@@ -65,7 +104,7 @@ export default function Home() {
           <br />
           <ul>
             <li>
-            <Image alt="imagem de um celular conectado a internet" width={100} height={100} src={"/img/internet.png"}></Image>
+              <Image alt="imagem de um celular conectado a internet" width={100} height={100} src={"/img/internet.png"}></Image>
               <p><strong>Informática para Internet</strong></p>
               <p>Curso Técnico</p>
               <p>C.E.E.P. José Figueireiro Barreto</p>
@@ -74,7 +113,7 @@ export default function Home() {
 
             <li>
               <Image alt="imagem de um computador" width={100} height={100} src={"/img/computer.png"}></Image>
-              <p><strong>Análise e Desenvolvimeto de Sistemas</strong></p>  
+              <p><strong>Análise e Desenvolvimeto de Sistemas</strong></p>
               <p>Curso Superior</p>
               <p>Instituto Federal de Sergipe</p>
               <p>Ano de Conclusão: 2024</p>
@@ -83,51 +122,60 @@ export default function Home() {
         </div>
       </Section>
 
-      <Section id="tech" color="black" flexDirection={"column"}> 
+      <Section id="tech" color="black" flexDirection={"column"}>
         <div className={styles.containerTech}>
-          <h2 className={styles.colorWhite} style={{fontSize: "25pt"}} >Tecnologias</h2>
-          <div>          
-            <Image alt="logo html" width={70} height={70} src="/img/html-5.png"/> 
-            <Image alt="logo css" width={70} height={70} src="/img/css-3.png"/>
-            <Image alt="logo javascript" width={70} height={70} src="/img/js.png"/>
-            <Image alt="logo react" width={70} height={70} src="/img/react.png"/>
-            <Image alt="logo nextjs" width={70} height={70} src="/img/next.png"/>
-            <Image alt="logo nodejs" width={70} height={70} src="/img/nodejs.png"/>
-            <Image alt="logo java" width={70} height={70} src="/img/java.png"/>
-            <Image alt="logo spring" width={70} height={70} src="/img/spring.png"/> 
-            <Image alt="logo mysql" width={70} height={70} src="/img/mysql.png"/>
+          <h2 className={styles.colorWhite} style={{ fontSize: "25pt" }} >Tecnologias</h2>
+          <div>
+            <Image alt="logo html" width={70} height={70} src="/img/html-5.png" />
+            <Image alt="logo css" width={70} height={70} src="/img/css-3.png" />
+            <Image alt="logo javascript" width={70} height={70} src="/img/js.png" />
+            <Image alt="logo react" width={70} height={70} src="/img/react.png" />
+            <Image alt="logo nextjs" width={70} height={70} src="/img/next.png" />
+            <Image alt="logo nodejs" width={70} height={70} src="/img/nodejs.png" />
+            <Image alt="logo java" width={70} height={70} src="/img/java.png" />
+            <Image alt="logo spring" width={70} height={70} src="/img/spring.png" />
+            <Image alt="logo mysql" width={70} height={70} src="/img/mysql.png" />
           </div>
         </div>
       </Section>
 
-      <Section id="project" color="#1b1b1b" flexDirection={"column"}>
-        <div className={styles.containerProject}>
-          <h2 className={styles.colorWhite} style={{fontSize: "25pt" }}>Projetos</h2>
-          <div> 
-            <CardTech img="/img/tela-netflix.png" title="Clone Página Netflix" description="Um clone da landing page da Netflix criado somente com HTML e CSS.">
-              <div>
-                <Link target="_blank" href="https://github.com/thiagodevcodes/netflix">Código Fonte</Link>
-                <Link target="_blank" href="https://streamnetclone.netlify.app/">Acessar</Link>
-              </div>
-            </CardTech>
+      <section id="project" style={{backgroundColor: "#1b1b1b"}}>
+          <div className={styles.sliderContainer}>
+          <h2 className={styles.colorWhite} style={{ fontSize: "25pt" }} >Projetos</h2>
 
-            <CardTech img="/img/tela-orcamento.png" title="Sistema de Orçamento" description="Uma aplicação web para orçamentos públicos feita com Java, Spring Boot, React e NextJS.">
-              <div>
-                <Link target="_blank" href="https://github.com/thiagodevcodes/orcamento-publico">Código Fonte</Link>
-                <Link target="_blank" href="https://orcamento-publico.vercel.app/">Acessar</Link>
-              </div> 
-            </CardTech>
+            <div className={styles.slider}>
+              <Slider {...settings}>
+                <div className={styles.itemSlide}>
+                  <CardTech img="/img/tela-netflix.png" title="Clone Página Netflix" description="Um clone da landing page da Netflix criado somente com HTML e CSS.">
+                    <div>
+                      <Link target="_blank" href="https://github.com/thiagodevcodes/netflix">Código Fonte</Link>
+                      <Link target="_blank" href="https://streamnetclone.netlify.app/">Acessar</Link>
+                    </div>
+                  </CardTech>
+                </div>
 
-            <CardTech img="/img/tela-portfolio.png" title="Portifólio" description="Um portifolio de apresentação utilizando as tecnologias de front-end NextJS, React, JavaScript e CSS.">
-              <div>
-                <Link target="_blank" href="https://github.com/thiagodevcodes/portifolio-thiago">Código Fonte</Link>
-                <Link target="_blank" href="https://thiagodev.vercel.app/">Acessar</Link>
-              </div>
+                <div className={styles.itemSlide}>
+                  <CardTech img="/img/tela-orcamento.png" title="Sistema de Orçamento" description="Uma aplicação web para orçamentos públicos feita com Java, Spring Boot, React e NextJS.">
+                    <div>
+                      <Link target="_blank" href="https://github.com/thiagodevcodes/orcamento-publico">Código Fonte</Link>
+                      <Link target="_blank" href="https://orcamento-publico.vercel.app/">Acessar</Link>
+                    </div>
+                  </CardTech>
+                </div>
 
-            </CardTech>
+                <div className={styles.itemSlide}>
+                  <CardTech img="/img/tela-portfolio.png" title="Portifólio" description="Um portifolio de apresentação utilizando as tecnologias de front-end NextJS, React, JavaScript e CSS.">
+                    <div>
+                      <Link target="_blank" href="https://github.com/thiagodevcodes/portifolio-thiago">Código Fonte</Link>
+                      <Link target="_blank" href="https://thiagodev.vercel.app/">Acessar</Link>
+                    </div>
+                  </CardTech>
+                </div>
+              </Slider>
+            </div>
           </div>
-        </div>
-      </Section>
+
+      </section>
 
       <Section id="contact" >
         <div className={styles.containerContact}>
@@ -146,7 +194,7 @@ export default function Home() {
             </Link>
             <Link target="_blank" href="https://www.linkedin.com/in/thiagosilvaweb/">
               <Image alt="link do perfil no linkedin" width={50} height={50} src="/img/linkedin.png" />
-            </Link>     
+            </Link>
           </div>
         </div>
       </Section>
