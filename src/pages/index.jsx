95b035b331d5d567287beb4f69f35c858b1eec0e
thiago <euthiagosrodrigues@gmail.com>
2024-02-07 03:45:02 -0300

    fix: consertar bug tamanho slider

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -62,17 +62,6 @@ export default function Home() {
       <Head>
         <title>Portifolio - Thiago Silva</title>
         <link rel="icon" href="/img/tag-html.png" type="image/x-icon"></link>
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charset="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
       </Head>
 
       <Header>
@@ -164,7 +153,7 @@ export default function Home() {
                 </div>
 
                 <div className={styles.itemSlide}>
-                  <CardTech img="/img/tela-portfolio.png" title="Portifólio" description="Um portifolio de apresentação utilizando as tecnologias de front-end NextJS, React, JavaScript e CSS.">
+                  <CardTech img="/img/tela-portfolio.png" title="Portifólio" description="Um portifolio utilizando as tecnologias de front-end NextJS, React, JavaScript e CSS.">
                     <div>
                       <Link target="_blank" href="https://github.com/thiagodevcodes/portifolio-thiago">Código Fonte</Link>
                       <Link target="_blank" href="https://thiagodev.vercel.app/">Acessar</Link>
